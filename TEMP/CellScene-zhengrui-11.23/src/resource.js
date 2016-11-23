f9a1a82ce5a7f4d1faafe843d573232db7135854
zhengrui2016 <1498162877@qq.com>
2016-11-23 17:16:51 +0800

    细胞介绍详情场景及图片

---
+++ TEMP/CellScene-zhengrui-11.23/src/resource.js
@@ -0,0 +1,36 @@
+var res = {
+    startbg : "res/StartImages/startbg.png",
+    startbtn : "res/StartImages/startbtn.png",
+    aboutusbtn : "res/StartImages/aboutus.png",
+    pokedexbg : "res/PokedexImages/pokedexbg.jpg",
+    poketumour : "res/PokedexImages/poketumour.png",
+    pokecell : "res/PokedexImages/pokecell.png",
+    pokecellbtn : "res/PokedexImages/pokecellbutton.png",
+    poketumourbtn : "res/PokedexImages/poketumourbutton.png",
+    pokereturnbtn : "res/PokedexImages/returnbutton.png",
+    display1:"res/PokedexImages/display1.jpg",
+    display2:"res/PokedexImages/display2.jpg",
+    celldisplay:"res/CellImages/celldisplay.png",
+    celldisplay2:"res/CellImages/celldisplay2.png",
+    cellgrid:"res/CellImages/cellgrid.png",
+    blackboard:"res/CellImages/blackboard.png",
+    cell1:"res/CellImages/cell1.png",
+    cell2:"res/CellImages/cell2.png",
+    cell3:"res/CellImages/cell3.png",
+    cell4:"res/CellImages/cell4.png",
+    cell5:"res/CellImages/cell5.png",
+    cell6:"res/CellImages/cell6.png",
+    cell7:"res/CellImages/cell7.png",
+    tumour1:"res/TumourImages/tumour1.png",
+    tumour2:"res/TumourImages/tumour2.png",
+    tumour3:"res/TumourImages/tumour3.png",
+    tumour4:"res/TumourImages/tumour4.png",
+    tumour5:"res/TumourImages/tumour5.png",
+    tumour6:"res/TumourImages/tumour6.png",
+    tumour7:"res/TumourImages/tumour7.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
