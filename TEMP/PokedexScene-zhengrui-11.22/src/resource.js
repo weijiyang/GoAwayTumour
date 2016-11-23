edeffa9466594d6b5a4f5e78f50583c6c2acafd5
zhengrui2016 <1498162877@qq.com>
2016-11-23 17:12:32 +0800

    图鉴场景及图片

---
+++ TEMP/PokedexScene-zhengrui-11.22/src/resource.js
@@ -0,0 +1,20 @@
+var res = {
+    startbg : "res/StartImages/startbg.png",
+    startbtn : "res/StartImages/startbtn.png",
+    aboutusbtn : "res/StartImages/aboutus.png",
+    pokedexbg : "res/PokedexImages/pokedexbg.jpg",
+    poketumour : "res/PokedexImages/poketumour.png",
+    pokecell : "res/PokedexImages/pokecell.png",
+    pokecellbtn : "res/PokedexImages/pokecellbutton.png",
+    poketumourbtn : "res/PokedexImages/poketumourbutton.png",
+    pokereturnbtn : "res/PokedexImages/returnbutton.png",
+    display1:"res/PokedexImages/display1.jpg",
+    display2:"res/PokedexImages/display2.jpg",
+    celldisplay:"res/CellImages/celldisplay.png",
+    cellgrid:"res/CellImages/cellgrid.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
