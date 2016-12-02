32080793609e48225ed2d863dcbe95639409b5b7
wangkaiming000 <1758477798@qq.com>
2016-12-02 21:18:21 +0800

    细胞子类的发现病菌、瞄准并射击功能

---
+++ TEMP/cellfunction-wangkaiming-12.2/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    Xue_png : "res/xue.png",
+    Bottle : "res/cell/Bottle_3.png",
+    Cell : "res/cell/Cell2.png",
+    PBottle : "res/cell/PBottle31.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
