10139a2e64086adbf04cf19b8e8b5be0301affd0
wangkaiming000 <1758477798@qq.com>
2016-12-13 09:45:58 +0800

    对游戏主场景背景音乐的改变优化

---
+++ TEMP/changemainbgmusic-wangkaiming-12.13/resource.js
@@ -0,0 +1,200 @@
+var res = {
+    //魏继阳
+    Level:"res/mainScene/level.jpg",
+    Level_up:"res/mainScene/up.png",
+    Level_close:"res/mainScene/close.png",
+    Cell:"res/mainScene/cell.png",
+    Level_up_down:"res/mainScene/up_down.png",
+    Level_close_down:"res/mainScene/close_down.png",
+    Cell_1:"res/mainScene/cell_1.png",
+    Cell_2:"res/mainScene/cell_2.png",
+    Cell_3:"res/mainScene/cell_3.png",
+    //郑蕊
+    startbg : "res/StartImages/startbg.png",
+    startbtn : "res/StartImages/startbtn.png",
+    startbtn2 : "res/StartImages/startbtn2.png",
+    aboutusbtn : "res/StartImages/aboutus.png",
+    aboutusbtn2 : "res/StartImages/aboutus2.png",
+    pokedexbg : "res/PokedexImages/pokedexbg2.png",
+    poketumour : "res/PokedexImages/poketumour.png",
+    poketumour2 : "res/PokedexImages/poketumour2.png",
+    pokecell : "res/PokedexImages/pokecell.png",
+    pokecell2 : "res/PokedexImages/pokecell2.png",
+    pokecellbtn : "res/PokedexImages/pokecellbutton.png",
+    pokecellbtn2 : "res/PokedexImages/pokecellbutton2.png",
+    poketumourbtn : "res/PokedexImages/poketumourbutton.png",
+    poketumourbtn2 : "res/PokedexImages/poketumourbutton2.png",
+    pokereturnbtn : "res/PokedexImages/returnbutton.png",
+    display1:"res/PokedexImages/display1.jpg",
+    display2:"res/PokedexImages/display2.jpg",
+    science11:"res/CellImages/sciencebg1.png",
+    science12:"res/CellImages/sciencebg1.png",
+    science21:"res/CellImages/sciencebg2.png",
+    science22:"res/CellImages/sciencebg2.png",
+    cellsciencebg1:"res/cellscience/lymphocyte.png",
+    cellsciencebg2:"res/cellscience/granulocyte.png",
+    cellsciencebg3:"res/cellscience/erythrocyte.png",
+    cellsciencebg4:"res/cellscience/phagocyte.png",
+    cellsciencebg5:"res/cellscience/NKcell.png",
+    cellsciencebg6:"res/cellscience/epicyte.png",
+    cellsciencebg7:"res/cellscience/phlogocyte.png",
+    tumoursciencebg1:"res/tumourscience/bacillusproteus.png",//变形杆菌
+    tumoursciencebg2:"res/tumourscience/cancercell.png",//癌细胞
+    tumoursciencebg3:"res/tumourscience/colibacillus.png",//大肠杆菌
+    tumoursciencebg4:"res/tumourscience/spirochete.png",//螺旋菌
+    tumoursciencebg5:"res/tumourscience/staphylococcus.png",//葡萄球菌
+    tumoursciencebg6:"res/tumourscience/streptococcus.png",//链球菌
+    tumoursciencebg7:"res/tumourscience/virus.png",//病毒
+    /*    celldisplay:"res/CellImages/celldisplay.png",
+     cellgrid:"res/CellImages/cellgrid.png",*/
+    //崔迎春
+    stagemap_toolbar_home:"res/ChooseLevel/stagemap_toolbar_home.png",
+    stagemap_toolbar_home2:"res/ChooseLevel/stagemap_toolbar_home2.png",
+    stagemap_toolbar_leaderboard:"res/ChooseLevel/stagemap_toolbar_leaderboard.png",
+    stagemap_toolbar_leaderboard2:"res/ChooseLevel/stagemap_toolbar_leaderboard2.png",
+    stagemap_toolbar_leftbg:"res/ChooseLevel/stagemap_toolbar_leftbg.png",
+    stagemap_toolbar_overten:"res/ChooseLevel/stagemap_toolbar_overten.png",
+    stagemap_toolbar_rightbg:"res/ChooseLevel/stagemap_toolbar_rightbg.png",
+    stagemap_toolbar_shop:"res/ChooseLevel/stagemap_toolbar_shop.png",
+    stagemap_toolbar_shop2:"res/ChooseLevel/stagemap_toolbar_shop2.png",
+    stagemap_toolbar_timer_normal:"res/ChooseLevel/stagemap_toolbar_timer_normal.png",
+    stagepoint_adv:"res/ChooseLevel/stagepoint_adv.png",
+    stagepoint_chance:"res/ChooseLevel/stagepoint_chance2.png",
+    stagepoint_chance2:"res/ChooseLevel/stagepoint_chance22.png",
+    stagepoint_gate:"res/ChooseLevel/stagepoint_gate.png",
+    stagepoint_time:"res/ChooseLevel/stagepoint_time.png",
+    stage_map_0:"res/ChooseLevel/Map/stage_map_0.png",
+    //王凯名
+    Music1_png : "res/music/music_1.png",
+    Music2_png : "res/music/music_2.png",
+    Music3_png : "res/music/music_3.png",
+    Music4_png : "res/music/music_4.png",
+    Mainscene_png : "res/music/mainscene.png",
+    //音乐与音效
+    Bg_mp3:"res/music/BGMusic01.mp3",
+    Click_mp3:"res/music/click.mp3",
+    BgMusic:"res/music/BGMusic.mp3",
+    Select:"res/music/Select.mp3",
+    //细胞部分:郑蕊和王凯名
+    /*    XiBao3_png : "res/celldetailScene/xibao3.png",
+     XiBao32_png : "res/celldetailScene/xibao3_2.png",*/
+    celldisplay:"res/CellImages/celldisplay12.png",
+    celldisplay2:"res/CellImages/celldisplay22.png",
+    cellgrid:"res/CellImages/cellgrid.png",
+    blackboard:"res/CellImages/blackboard.png",
+    cell1:"res/CellImages/cell1_2.png",
+    cell2:"res/CellImages/cell2_2.png",
+    cell3:"res/CellImages/cell3_2.png",
+    cell4:"res/CellImages/cell4_2.png",
+    cell5:"res/CellImages/cell5_2.png",
+    cell6:"res/CellImages/cell6_2.png",
+    cell7:"res/CellImages/cell7_2.png",
+    cell12:"res/CellImages/cell1.png",
+    cell22:"res/CellImages/cell2.png",
+    cell32:"res/CellImages/cell3.png",
+    cell42:"res/CellImages/cell4.png",
+    cell52:"res/CellImages/cell5.png",
+    cell62:"res/CellImages/cell6.png",
+    cell72:"res/CellImages/cell7.png",
+    tumour1:"res/TumourImages/tumour1.png",
+    tumour2:"res/TumourImages/tumour2.png",
+    tumour3:"res/TumourImages/tumour3.png",
+    tumour4:"res/TumourImages/tumour4.png",
+    tumour5:"res/TumourImages/tumour5.png",
+    tumour6:"res/TumourImages/tumour6.png",
+    tumour7:"res/TumourImages/tumour7.png",
+    tumour12:"res/TumourImages/tumour1_2.png",
+    tumour22:"res/TumourImages/tumour2_2.png",
+    tumour32:"res/TumourImages/tumour3_2.png",
+    tumour42:"res/TumourImages/tumour4_2.png",
+    tumour52:"res/TumourImages/tumour5_5.png",
+    tumour62:"res/TumourImages/tumour6_2.png",
+    tumour72:"res/TumourImages/tumour7_2.png",
+    //曹阳
+    // BackgoundImage:"res/mainScene/background.png",
+    BGS:"res/mainScene/9.tmx",
+    plus:"res/mainScene/play/select_01.png",
+    cell1_png:"res/mainScene/play/Tower/cell1.png",
+    cell2_png:"res/mainScene/play/Tower/cell2.png",
+    minus:"res/mainScene/play/warning.png",
+    BackgoundImage:"res/mainScene/1.png",
+    pressed:"res/CellImages/share_setting_pressed.png",
+    pressed2:"res/CellImages/share_setting_pressed_2.png",
+    //张东杰
+    blood:"res/mainScene/blood.png",
+    bloodBar:"res/mainScene/bloodBar.png",
+    HelloWorld_png : "res/HelloWorld.png",
+    topBg_png:"res/mainScene/top_bg.png",
+    wavesBg_png:"res/mainScene/waves_bg.png",
+    abc0_png:"res/mainScene/abc0.png",
+    abc1_png:"res/mainScene/abc1.png",
+    speed0_png:"res/mainScene/speed_0.png",
+    speed1_png:"res/mainScene/speed_1.png",
+    Tan_png : "res/mainScene/tan.png",
+    bnt : 'res/mainScene/bnt.png',
+    pause_0 : 'res/mainScene/pause_0.png',
+    pause_1 : 'res/mainScene/pause_1.png',
+    menu_png : 'res/mainScene/menu.png',
+    List_png : 'res/mainScene/list.png',
+    About_png:"res/mainScene/about.png",
+    GameOver_png:"res/mainScene/gameover.png",
+    Win_png:"res/mainScene/win.png",
+    ContinueNormal_png:"res/mainScene/continuenormal.png",
+    ContinueSelected_png:"res/mainScene/continueselected.png",
+    SelectNormal_png:"res/mainScene/selectnormal.png",
+    SelectSelected_png:"res/mainScene/selectselected.png",
+    RepeatNormal_png:"res/mainScene/repeatnormal.png",
+    RepeatSelected_png:"res/mainScene/repeatsected.png",
+    Star1_png:"res/mainScene/star1.png",
+    Star2_png:"res/mainScene/star2.png",
+    Star3_png:"res/mainScene/star3.png",
+    GrayStar1_png:"res/mainScene/graystar1.png",
+    GrayStar2_png:"res/mainScene/graystar2.png",
+    GrayStar3_png:"res/mainScene/graystar3.png",
+
+    Bottleplist_plist:"res/mainScene/play/Tower/Bottle.plist",
+    // Bottle_01:"res/mainScene/play/Tower/Bottle/Bottle01.png",
+    Bottle_png:"res/mainScene/play/Tower/Bottle.png",
+
+    tc : 'res/mainScene/tc.png'
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
+
+var gameres={
+
+};
+var g_gamePlay_resources = [];
+for (var j in res) {
+    g_gamePlay_resources.push(gameres[j]);
+}
+/*g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle.plist");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle.png");*/
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle01.png");
+
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle_3.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle00.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle11.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle12.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle13.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle21.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle22.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle23.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle31.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle32.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/Bottle33.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle01.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle02.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle11.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle12.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle13.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle21.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle22.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle23.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle31.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle32.png");
+g_gamePlay_resources.push("res/mainScene/play/Tower/Bottle/PBottle33.png");
+
