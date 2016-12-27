55e74af5c766c9715f1f377537331900c55be3b0
zhengrui2016 <1498162877@qq.com>
2016-12-27 12:14:50 +0800

    update

---
+++ TEMP/celldupdate-zhengrui-12.26/cellscience/cellfive.js
@@ -0,0 +1,48 @@
+/**
+ * Created by DELL on 2016/12/4.
+ */
+/**
+ * Created by DELL on 2016/12/4.
+ */
+
+
+var CellfiveLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        this._super();
+        var size = cc.winSize;
+        //图鉴背景图层
+        var pokedexbg = new cc.Sprite(res.cellsciencebg5);
+        pokedexbg.x=size.width*0.5;
+        pokedexbg.y=size.height*0.5;
+        //pokedexbg.setAnchorPoint(0,0);
+        this.addChild(pokedexbg);
+
+        //返回按钮
+        var press= new cc.MenuItemImage(res.pressed, res.pressed2, function () {
+            //王凯名 添加了音效判断2016/12/8
+            EffectEngine.playEffect(res.Select);
+            //详情介绍接口
+            // cc.log("显示详情");
+            cc.director.runScene(new CellDetailScene());
+        }, this);
+        press.x = pokedexbg.width*0.1;
+        press.y = pokedexbg.height*0.93;
+        var menu0 = new cc.Menu(press);
+        menu0.x = 0;
+        menu0.y = 0;
+        pokedexbg.addChild(menu0);
+
+        return true;
+    }
+});
+
+var CellfiveScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new CellfiveLayer();
+        this.addChild(layer);
+    }
+});
+
+
